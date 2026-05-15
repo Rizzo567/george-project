bae2e7807c5ca2a63e620e9ff479f3ec8f4d8a6d
Rizzo567 <manuelrizzo474@gmail.com>
2026-05-15 20:30:47 +0200

    config: imposta credenziali Supabase reali

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,5 +1,5 @@
 window.MB_CONFIG = {
-  SUPABASE_URL:  'INSERISCI_PROJECT_URL',
-  SUPABASE_KEY:  'INSERISCI_ANON_KEY',
-  EDGE_FN_URL:   'INSERISCI_PROJECT_URL/functions/v1/send-notification'
+  SUPABASE_URL:  'https://ccmpysycifufktbrkiot.supabase.co',
+  SUPABASE_KEY:  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNjbXB5c3ljaWZ1Zmt0YnJraW90Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg4NjA3NzYsImV4cCI6MjA5NDQzNjc3Nn0.G0qWDUmFHGuVsEqX3TqbW0ztyqxTwyyoPYqmluXGAMA',
+  EDGE_FN_URL:   'https://ccmpysycifufktbrkiot.supabase.co/functions/v1/send-notification'
 };
